b7685defb294af85e81d441b61615e53dcd07258
Tyler J. Otte <TylerJOtte@icloud.com>
2021-10-15 19:35:32 -0500

    Add Color extension
    This provides an extension for common Color operations & scheme Colors.

---
+++ src/TwentyNine/TwentyNine/Extensions/ColorExtension.swift
@@ -0,0 +1,121 @@
+//=============================================================================//
+//                                                                             //
+//  ColorExtension.swift                                                       //
+//  29                                                                         //
+//                                                                             //
+//  Created by Tyler J. Otte on 19/04/21.                                      //
+//-----------------------------------------------------------------------------//
+//                                                                             //
+// This source file is part of the 29 project.                                 //
+//                                                                             //
+// Copyright (c) 2021, Tyler J. Otte.                                          //
+// Licensed under the GNU Affero General Public License v3.0.                  //
+//                                                                             //
+// See https://github.com/TylerJOtte/29/LICENSE.txt for more details.          //
+//=============================================================================//
+
+import Foundation
+import SwiftUI
+
+/// An extension  for common `Color` operations.
+internal extension Color {
+    
+    //=========================================================================//
+    //                                ATTRIBUTES                               //
+    //=========================================================================//
+    
+    /// Scheme colors
+    static let darkGrey: Color = Color(hex: "454545")
+    static let darkRed: Color = Color(hex: "581500")
+    static let green: Color = Color(hex: "006600")
+    static let lightGrey: Color = Color(hex: "808080")
+    static let red: Color = Color(hex: "AF2A00")
+    static let tan: Color = Color(hex: "EFECCA")
+    
+    // Scheme gradients
+    static let redGradient = getGradient(red, darkRed)
+    static let whiteGradient = getGradient(.white, lightGrey)
+    
+    //=========================================================================//
+    //                              CONSTRUCTORS                               //
+    //=========================================================================//
+    
+    /// Creates a `Color` with the given HEX value.
+    ///
+    /// - Precondition: The given value must be a valid 6-character HEX value.
+    /// - Postcondition: A `Color` is created from the given HEX value.
+    /// - Parameter hex: The 6-character HEX value to create `Color` for.
+    init(hex: String) {
+        
+        let characterSet = CharacterSet.alphanumerics.inverted
+        let value = hex.trimmingCharacters(in: characterSet)
+        var base: UInt64 = 0
+        
+        Scanner(string: value).scanHexInt64(&base)
+        let alpha, red, green, blue: UInt64
+        
+        switch value.count {
+        
+        case 3: // RGB (12-bit)
+            (alpha, red, green, blue) = (
+                255,
+                (base >> 8) * 17,
+                (base >> 4 & 0xF) * 17,
+                (base & 0xF) * 17
+            )
+            
+        case 6: // RGB (24-bit)
+            (alpha, red, green, blue) = (
+                255,
+                base >> 16,
+                base >> 8 & 0xFF,
+                base & 0xFF
+            )
+            
+        case 8: // ARGB (32-bit)
+            (alpha, red, green, blue) = (
+                base >> 24,
+                base >> 16 & 0xFF,
+                base >> 8 & 0xFF,
+                base & 0xFF)
+            
+        default:
+            (alpha, red, green, blue) = (1, 1, 1, 0)
+        }
+
+        self.init(
+            .sRGB,
+            red: Double(red) / 255,
+            green: Double(green) / 255,
+            blue:  Double(blue) / 255,
+            opacity: Double(alpha) / 255
+        )
+    }
+    
+    //=========================================================================//
+    //                                 GETTERS                                 //
+    //=========================================================================//
+    
+    /// Retrieve a `LindearGradient` with the given colors and coordinates.
+    ///
+    /// - Precondition: None.
+    /// - Postcondition: None.
+    /// - Parameters:
+    ///   - startColor: The `Color` to commence gradent with.
+    ///   - endColor: The `Color` to end gradient with.
+    ///   - x: The start point's x-axis point.
+    ///   - y: The start point's y-axis point.
+    ///   - x2: The end point's x-axis point.
+    ///   - y2: The end point's y-axis point.
+    /// - Returns: A `LinearGradient` for the given terms.
+    private static func getGradient(_ startColor: Color, _ endColor: Color,
+                                    _ x: CGFloat = 0, _ y: CGFloat = 0,
+                                    _ x2: CGFloat = 0.5, _ y2: CGFloat = 0.6) ->
+                                    LinearGradient {
+     
+        return LinearGradient(
+            gradient:   .init(colors: [startColor, endColor]),
+            startPoint: .init(x: x, y: y),
+            endPoint:   .init(x: x2, y: y2))
+    }
+}
--- src/TwentyNine/TwentyNine/TwentyNineApp.swift
+++ src/TwentyNine/TwentyNine/TwentyNineApp.swift
@@ -1,20 +1,36 @@
-//
-//  TwentyNineApp.swift
-//  TwentyNine
-//
-//  Created by Tyler J. Otte on 9/25/21.
-//
+//=============================================================================//
+//                                                                             //
+//  TwentyNineApp.swift                                                        //
+//  29                                                                         //
+//                                                                             //
+//  Created by Tyler J. Otte on 9/25/21.                                       //
+//-----------------------------------------------------------------------------//
+//                                                                             //
+// This source file is part of the 29 project.                                 //
+//                                                                             //
+// Copyright (c) 2021, Tyler J. Otte.                                          //
+// Licensed under the GNU Affero General Public License v3.0.                  //
+//                                                                             //
+// See https://github.com/TylerJOtte/29/LICENSE.txt for more details.          //
+//=============================================================================//
+
 
 import SwiftUI
 
+/// The application's main entry point.
 @main
 struct TwentyNineApp: App {
-    let persistenceController = PersistenceController.shared
 
+    //=========================================================================//
+    //                                ATTRIBUTES                               //
+    //=========================================================================//
+    
+    @StateObject private var modelData: ModelData = ModelData()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+                .environmentObject(modelData)
         }
     }
 }
